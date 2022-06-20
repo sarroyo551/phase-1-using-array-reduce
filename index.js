1e4132812194a4adf85cbe538826bd5c15995ae7
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-20 17:43:34 -0400

    commit tree 9182b63a89ef57f4b8d70d8c62359f290d6c47b8

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+/*function addTogether (a, b) {
+    return a + b
+}*/
+function addTogether(acc, element) {
+    return acc + element;
+}
+const totalBatteries = batteryBatches.reduce(addTogether, 0)
+
+
+
